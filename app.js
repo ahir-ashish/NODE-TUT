80c73f4a2e6ae1111ea126fe0a65d106e9e5b876
ahir-ashish <ashishjotva1688@gmail.com>
2024-02-29 17:14:56 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const data = [
+    {name: 'ashish', Number: "9725853385", email: "ashish@gmail.com",passwod:"9874561230"},
+   { name: 'kaushik', Number: "9725853385", email: "ashish@gmail.com" },
+   { name: 'raj', Number: "9725853385", email: "ashish@gmail.com" },
+   { name: 'sunil', Number: "9725853385", email: "ashish@gmail.com" }
+]
+module.exports = data;    
---
+++ index.js
@@ -0,0 +1,9 @@
+/*const http = require("http");
+const data = require("./app");
+http.createServer((req, res) => {
+    res.writeHead(200, {'content-type':'application/json' });
+    res.write(JSON.stringify(data));
+    res.end();
+}).listen(4000)*/
+
+console.log(process.argv[2]);
